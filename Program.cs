16d02edec91b465a016fc1c93ccf0a3315fe3dcd
Pawel.Kam <49959705+K44myczek@users.noreply.github.com>
2019-11-22 19:36:05 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interfejss
+{
+    class Program 
+    {
+        
+    public static  osoba uzupelnianie()
+        {
+            Console.WriteLine("Podaj imię");
+            string imie = Console.ReadLine();
+            Console.WriteLine("Podaj nazwisko");
+            string nazwisko = Console.ReadLine();
+            osoba test = new osoba(imie, nazwisko);
+            Console.Clear();
+
+            return test;
+        }
+        static void Main(string[] args)
+        {
+            List<osoba> lista = new List<osoba>();
+
+
+            for (int i = 0; i < 2; i++)
+            {
+                lista.Add(uzupelnianie());   
+            }
+            lista.Sort();
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine($"Nazywam się {lista[i].Imie} {lista[i].Nazwisko}" );
+            }
+        }
+    }
+}
---
+++ osoba.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfejss
+{
+    class osoba :IComparable<osoba>
+    {
+        private string imie;
+        private string nazwisko;
+
+        public string Imie { get => imie; set => imie = value; }
+        public string Nazwisko { get => nazwisko; set => nazwisko = value; }
+        public osoba(string imie, string nazwisko)
+        {
+            this.imie=imie;
+            this.nazwisko = nazwisko;
+        }
+
+        public int CompareTo(osoba other)
+        {
+            int result = 0;
+            result = nazwisko.CompareTo(other.nazwisko);
+            if (result==0)
+            {
+                result = nazwisko.CompareTo(other.nazwisko);
+            }
+            return result;
+        }
+    }
+}
